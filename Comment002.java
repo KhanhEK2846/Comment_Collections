870b1391b9556123bc3b2bd972869c657d3334d0
KhanhEK <107037350+KhanhEK2846@users.noreply.github.com>
2024-02-21 20:30:48 +0700

    Something

--- Comment002.java
+++ Comment002.java
@@ -1,6 +1,6 @@
- // 
- //
- //
+ // Xin chào,
+ // Khi tôi bắt đầu thực hiện dự án này, chỉ có Chúa và tôi biết cách chạy nó
+ // Giờ thì chỉ có chúa mới biết
  //
  //           _.-/`)
  //          // / / )
